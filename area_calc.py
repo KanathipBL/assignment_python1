88cfd5a9c6a16d2e3340690788149aa592811367
KanathipBL <146318231+KanathipBL@users.noreply.github.com>
2023-09-28 21:31:34 +0700

    Add files via upload

---
+++ area_calc.py
@@ -0,0 +1,18 @@
+from area.rectangle import Rect
+from area.circle import Circle
+from area.Triangle import Tri
+from area.Ellipse import Ell
+
+if __name__ == '__main__':
+    area_1 = Rect(4, 8)
+    area_2 = Rect(6, 10)
+    area_3 = Circle(4)
+    area_4 = Tri(5,2)
+    area_5 = Ell(5,6)
+    print(area_1)
+    print(area_2)
+    print(area_3)
+    print(area_4)
+    print(area_5)
+
+#%%
